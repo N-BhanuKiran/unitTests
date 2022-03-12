d0f0a6e3f68843a4d65b4bc7ec52ab082a4d76db
bhanu kiran <bhanukiran@ymail.com>
2022-03-12 12:00:22 +0530

    added unit test cases

--- src/test/java/com/practise/unittests/UnittestsApplicationTests.java
+++ src/test/java/com/practise/unittests/UnittestsApplicationTests.java
@@ -1,13 +1,35 @@
 package com.practise.unittests;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.CollectionUtils;
 
 @SpringBootTest
 class UnittestsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Date date = new Date();
+		System.out.println(date);
+		System.out.println(new Date().compareTo(new Date()));
+		assertThat(date).isNotNull();	
+	}
+
+	@Test
+	void collectionTest() {
+		List<String> users = new ArrayList<String>();
+		if (CollectionUtils.isEmpty(users)) {
+			users.add("arg0");
+		} else {
+			System.out.println("this will never be printed");
+		}
+		assertThat(users.size()).isGreaterThan(0);
 	}
 
 }
